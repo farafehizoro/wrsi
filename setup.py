7a6ede1ca15eea61eb61660dcd5922834a2d5760
Fehizoro <farafehizoro@gmail.com>
2022-12-25 18:16:41 +0300

    update setup file*

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name = 'wrsi',
       version = '0.1',
       description = 'Calculation of water requirement satisfaction index for crop',
-      packages = 'wrsi',
+      packages = ['wrsi'],
       zip_safe = False)
 
